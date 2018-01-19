3ea569387723df8b1efc2d25f6243f86661eba86
gaoruxue <1573059736@qq.com>
2018-01-19 17:59:29 +0800

    1-19号，，vue-todo（axios）

---
+++ vue-todos/src/components/layouts.vue
@@ -0,0 +1,28 @@
+<template>
+<!-- 最外层的容器 -->
+  <section class="container">
+      <section class="menu">
+          <!-- 左边的容器 -->
+          <menus></menus>
+      </section>
+      <section class="content-container">
+        <!-- 右边的容器 -->
+        <todo></todo>
+      </section>
+  </section>
+</template>
+<script>
+import menus from './menus';
+import todo from './todo';
+export default {
+  components:{ //注册menus组件，让其可以在template调用
+      menus,
+      todo
+  }
+}
+</script>
+<style lang="less" scoped>
+@import url(../common/style/layouts.less);
+</style>
+
+
---
+++ vue-todos/src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue' //引入vue模块
+import App from './App' //引入vue组件
+import router from './router' //引入路由配置文件
+import Mock from './mock'; // 引入mock模块
+
+Mock.start(); //并且执行初始化函数
+Vue.config.productionTip = false //关闭生产模式下给出的提示
+
+/* eslint-disable no-new */
+new Vue({ //创建一个Vue的根实例
+    el: '#app', //挂载id，这个实例下的所有内容都会在index.html 一个为app的div下显示
+    router, //注入路由配置
+    components: { App }, //注入组件
+    template: '<App/>' //配置根模板，即打开页面显示那个组件
+})
